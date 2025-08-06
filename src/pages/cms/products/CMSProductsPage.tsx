c5fc41c432094ae880ad3de388774caf953ec6b8
L3me05 <lemueldimarcantonio.elite@gmail.com>
2025-08-06 13:01:58 +0200

    feature: creazione pagina CMSProductsPage.tsx con reducer per lo state

--- src/pages/cms/products/CMSProductsPage.tsx
+++ src/pages/cms/products/CMSProductsPage.tsx
@@ -1,9 +1,50 @@
+import {useReducer} from "react";
+import {get} from "../../../services/products/products.api.ts";
+import type {Product} from "../../../model/product.ts";
+import type {ProductsActions} from "../../../services/products/products.action.ts";
+
+export interface ProductsState {
+    products: Product[];
+    pending: boolean;
+}
+
+
+function productsReducer(state:ProductsState, action: ProductsActions) {
+    switch (action.type) {
+        case'pending':
+            return { ...state, pending: action.payload };
+        case 'productsGetSuccess':
+            return { pending: false, products:action.payload };
+    }
+    return state
+}
+
+
+const initialState: ProductsState = { pending: false, products: [] }
+
 export function CMSProductsPage() {
+    const [state, dispatch] = useReducer(productsReducer, initialState);
+
+    async function getProductHandler() {
+        dispatch({ type: 'pending', payload: true});
+        const res = await get();
+        dispatch({ type: 'productsGetSuccess', payload: res.items});
+    }
+
     return (
         <>
             <h1 className="title">CMSProducts</h1>
 
-            Products Page
+            <hr className="my-8"/>
+
+            { state.pending && <div>get products</div>}
+
+            <button
+                className="btn primary"
+                onClick={getProductHandler}
+            >get products</button>
+
+            <pre>{JSON.stringify(state, null, 2)}</pre>
         </>
     )
 }
