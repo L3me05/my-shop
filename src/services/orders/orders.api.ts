cefd4c0990a201b2ae5ca7bd68eb2b478358b19c
L3me05 <lemueldimarcantonio.elite@gmail.com>
2025-08-25 11:16:18 +0200

    feature: aggiunta orders api

---
+++ src/services/orders/orders.api.ts
@@ -0,0 +1,20 @@
+import {pb} from "../../pocketbase.ts";
+import type {Order} from "../../model/order.ts";
+import type {OrderForm, OrderStatus} from "../../model/order-form.ts";
+
+export async function get() {
+    return pb.collection('orders').getList<Order>();
+}
+
+export async function remove(id: string) {
+    return pb.collection('orders').delete(id);
+}
+
+export async function add(order: OrderForm) {
+    return pb.collection('orders').create<Order>(order);
+}
+
+export async function toggleStatus(id:string, status: OrderStatus) {
+    return pb.collection('orders').update<Order>(id, { status });
+}
+
