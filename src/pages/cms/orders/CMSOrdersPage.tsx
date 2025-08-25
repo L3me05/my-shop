0c9b5029429b8bcd13b322a9739bafbb264d1a52
L3me05 <lemueldimarcantonio.elite@gmail.com>
2025-08-25 13:04:40 +0200

    feature: creazione barrel file e UI CMSOrdersPage

--- src/pages/cms/orders/CMSOrdersPage.tsx
+++ src/pages/cms/orders/CMSOrdersPage.tsx
@@ -1,23 +1,64 @@
 import {useEffect} from "react";
-import {pb} from "../../../pocketbase.ts";
-import type {Order} from "../../../model/order.ts";
+import {useOrdersService} from "../../../services/orders";
+import {Spinner} from "../../../shared/components/core/Spinner.tsx";
+import {ServerError} from "../../../shared";
 
 export function CMSOrdersPage() {
 
+    const { state, getOrders, deleteOrder, addOrder, toggleOrderStatus } = useOrdersService();
+
     useEffect(() => {
-        getAll();
+        getOrders();
     }, []);
 
-    async function getAll() {
-        const res = await pb.collection('orders').getList<Order>()
-        console.log(res.items);
-    }
 
     return (
-        <>
+        <div>
+            { state.pending && <Spinner /> }
+            { state.error && <ServerError message={state.error} /> }
+
             <h1 className="title">CMSOrders</h1>
 
-            Orders Page
-        </>
+            <table className="border-collapse table-auto w-full my-12">
+                <thead>
+                    <tr>
+                        <th className="text-left">CLIENT / DATE</th>
+                        <th className="text-left">ORDER INFO</th>
+                        <th className="text-center">ACTIONS</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                {
+                    state.orders.map(item => {
+                        return (
+                            <tr className="h-24" key={item.id}>
+                                <td>
+                                    <div className="text-xl font-bold">{item.user.name}</div>
+                                    <div>{new Date(item.created).toDateString()}</div>
+                                </td>
+                                <td className="text-left">
+                                    <div>Total: € {item.total}</div>
+                                    <div>{item.order.length} products</div>
+                                </td>
+                                <td className="text-center">
+                                    {
+                                        item.status === 'pending' &&
+                                        <button className="btn primary" onClick={() => toggleOrderStatus(item.id, 'done')}>
+                                            Mark as Done
+                                        </button>
+                                    }
+                                    <button className="btn danger" onClick={() => deleteOrder(item.id)}>
+                                        <i className="fa fa-trash"></i>
+                                    </button>
+                                </td>
+                            </tr>
+                        )
+                    })
+                }
+                </tbody>
+            </table>
+
+        </div>
     )
 }
